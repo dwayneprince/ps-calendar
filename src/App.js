2c062c50af6aaa4d4a01e3132db81e67cb00c984
Dwayne Prince <dwayneprincere@gmail.com>
2024-01-31 18:41:50 -0500

    default

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/:year/:month" Component={Calendar} /> */}
-        <Route path="https://dwayneprince.github.io/ps-calendar/:year/:month" element={<Calendar />} />
 
-        <Route path="/*" element={<Navigate to="https://dwayneprince.github.io/ps-calendar/2023/6" />} />
+        <Route path="/:year/:month" element={<Calendar />} />
 
+        <Route path="/*" element={<Navigate to="/2023/6" />} />
       </Routes>
     </Router>
   );
