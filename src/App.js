3cf9bc49462a866bf61b54ef1a203f2644bc34eb
Dwayne Prince <dwayneprincere@gmail.com>
2024-01-31 18:32:52 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Calendar from './components/Calendar/Calendar';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/:year/:month" Component={Calendar} />
+        {/* <Route path="/:year/:month" Component={Calendar} /> */}
+        <Route path="/dwayneprince/ps-calendar.git/:year/:month" element={<Calendar />} />
+
+        <Route path="/*" element={<Navigate to="/dwayneprince/ps-calendar.git/2023/6" />} />
+
       </Routes>
     </Router>
   );
